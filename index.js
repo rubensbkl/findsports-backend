fa5e0e7d53b6c4a159753da951f24ba77161b18c
Rubens Dias <rubens.dias08@hotmail.com>
2024-07-01 00:21:21 -0300

    tamo indo

--- index.js
+++ index.js
@@ -64,13 +64,11 @@ server.post('/user-login', async (req, res) => {
 
 // User Update
 server.put('/user-edit', async (req, res) => {
-    console.log(req.body);
     const users = db.get('users');
     const updateData = req.body;
     const userId = updateData.id; // Renomeado para maior clareza
     if (userId) {
       const userExists = users.find({ id: userId }).value(); // Verifica se o usuário existe
-      console.log(userExists);
       if (userExists) {
         users.find({ id: userId }).assign(updateData).write();
         return res.status(200).json({ message: 'User updated successfully' });
@@ -110,9 +108,10 @@ server.post('/arena-register', async (req, res) => {
 
   // Default values
   newArena.id = arenas.size().value() + 1;
-
-  // Include the user ID in the new arena
   newArena.ownerId = req.userId; // use req.userId here
+  newArena.likes = 0;
+  newArena.dislikes = 0;
+  newArena.rating = 0;
 
   arenas.push(newArena).write();
 
@@ -125,23 +124,53 @@ server.get('/arenas', (req, res) => {
   res.status(200).json(arenas);
 });
 
+// Get Arena Profile
+server.get('/arena-profile/:id', (req, res) => {
+    const id = Number(req.params.id);
+    // Busque o perfil do usuário com esse ID do banco de dados
+    const arena = db.get('arenas').find({ id: id }).value();
+  
+    if (!arena) {
+      // Se não houver usuário com esse ID, envie um erro 404
+      return res.status(404).send('User not found');
+    }
+  
+    // Se houver um usuário, envie-o de volta como resposta
+    res.status(200).json(arena);
+});
+
 // Evento Register
 server.post('/evento-register', async (req, res) => {
-  const eventos = db.get('eventos');
-  const newEvento = req.body;
+  const events = db.get('events');
+  const newEvent = req.body;
 
   // Default values
-  newEvento.id = eventos.size().value() + 1;
+  newEvent.id = events.size().value() + 1;
 
-  eventos.push(newEvento).write();
+  events.push(newEvent).write();
 
   res.status(200).json({ message: 'Evento registered successfully' });
 });
 
-// Get All Eventos
-server.get('/eventos', (req, res) => {
-  const eventos = db.get('eventos').value();
-  res.status(200).json(eventos);
+// Get All Events
+server.get('/events', (req, res) => {
+  const events = db.get('events').value();
+  res.status(200).json(events);
+});
+
+// Get Arena Profile
+server.get('/event-profile/:id', (req, res) => {
+    const id = Number(req.params.id);
+    // Busque o perfil do usuário com esse ID do banco de dados
+    const event = db.get('events').find({ id: id }).value();
+  
+    if (!event) {
+      // Se não houver usuário com esse ID, envie um erro 404
+      return res.status(404).send('User not found');
+    }
+  
+    // Se houver um usuário, envie-o de volta como resposta
+    res.status(200).json(event);    
 });
 
 // Send Notification
@@ -163,7 +192,6 @@ server.post('/send-notification', async (req, res) => {
 // Adapted Get Notifications Endpoint
 server.get('/notifications', (req, res) => {
     const id = req.query.token;
-    console.log(id);
     let team = db.get('team-invite').value();
     let event = db.get('event-invite').value();
   
